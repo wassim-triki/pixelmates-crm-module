b1462a0a05d95ad4e0cbc41889ab74c3a4bec4ac
Wassim Triki <wsmtriki@gmail.com>
2025-02-18 19:02:48 +0100

    initial configuration and setup: react express server eslint prettier etc...

---
+++ backend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  js.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'off', // Disable the no-console rule
+    },
+  },
+  prettierConfig,
+];
