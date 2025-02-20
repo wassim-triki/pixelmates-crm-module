7014296e37fd1d012eed25389751f610d2bb5bbc
Wassim Triki <wsmtriki@gmail.com>
2025-02-20 19:19:08 +0100

    add user and role shcemas and permissions constants

---
+++ backend/src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    image: { type: String, required: false }, // URL to image
+    phone: { type: String, required: false },
+    address: { type: String, required: false },
+    birthday: { type: Date, required: false },
+    password: { type: String, required: true }, // Hashed password
+    role: { type: mongoose.Schema.Types.ObjectId, ref: 'Role' }, // Reference to Role
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Restaurant',
+      required: false,
+    }, // Only for Admins/Employees
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
