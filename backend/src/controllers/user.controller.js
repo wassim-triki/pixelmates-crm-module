931cfd0e51894a182be62066567b2082b4a3693a
Wassim Triki <wsmtriki@gmail.com>
2025-02-22 20:31:11 +0100

    add user controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const User = require('../models/user.model');
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
