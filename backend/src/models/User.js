a5136793d054b10ed34d4c4f06cc4d6937bbae53
Wassim Triki <wsmtriki@gmail.com>
2025-02-22 18:50:12 +0100

    revert to require syntax in express app

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+const { default: mongoose } = require('mongoose');
+
 const UserSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
