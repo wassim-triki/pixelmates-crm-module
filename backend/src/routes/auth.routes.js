e14f9347096711cfbbf742187769cbc3a8111d96
Wassim Triki <wsmtriki@gmail.com>
2025-02-23 20:29:51 +0100

    add auth controller and routes empty

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const authController = require('../controllers/auth.controller');
+const router = express.Router();
+
+// router.get('/', userController.getAllUsers);
