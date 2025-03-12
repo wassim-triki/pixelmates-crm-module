e4b7dd852a801fa7e3e8ce6365d7396163d51608
Wassim Triki <wsmtriki@gmail.com>
2025-03-12 02:54:52 +0100

    add allow self signed certif in nodemailer

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false, // Allows self-signed certificates REMOVE IN PRODUCTION
+  },
 });
 
 module.exports = async function sendEmail({ to, subject, template, data }) {
