754381d0a8002492778c9d9d992dceee0498bca9
Wassim Triki <wsmtriki@gmail.com>
2025-05-19 22:57:34 +0100

    fix: imports in NavHader hhh

--- back-office/src/jsx/layouts/nav/NavHader.jsx
+++ back-office/src/jsx/layouts/nav/NavHader.jsx
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 /// images
 import logo from '../../../assets/images/logo-officiel-menufy-long.png';
 
-export function NavMenuToggle() {
-  setTimeout(() => {
-    let mainwrapper = document.querySelector('#main-wrapper');
-    if (mainwrapper.classList.contains('menu-toggle')) {
-      mainwrapper.classList.remove('menu-toggle');
-    } else {
-      mainwrapper.classList.add('menu-toggle');
-    }
-  }, 200);
 export function NavMenuToggle() {
   setTimeout(() => {
     let mainwrapper = document.querySelector('#main-wrapper');
@@ -33,28 +22,27 @@ const NavHader = () => {
       <Link to="/" className="brand-logo">
         <img className="logo-abbr" src={logo} alt="Logo MenuFy" />
       </Link>
-  const [toggle, setToggle] = useState(false);
-  return (
-    <div className="nav-header">
-      <Link to="/" className="brand-logo">
-        <img className="logo-abbr" src={logo} alt="Logo MenuFy" />
-      </Link>
+      const [toggle, setToggle] = useState(false); return (
+      <div className="nav-header">
+        <Link to="/" className="brand-logo">
+          <img className="logo-abbr" src={logo} alt="Logo MenuFy" />
+        </Link>
 
-      <div
-        className="nav-control"
-        onClick={() => {
-          setToggle(!toggle);
-          NavMenuToggle();
-        }}
-      >
-        <div className={`hamburger ${toggle ? 'is-active' : ''}`}>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
+        <div
+          className="nav-control"
+          onClick={() => {
+            setToggle(!toggle);
+            NavMenuToggle();
+          }}
+        >
+          <div className={`hamburger ${toggle ? 'is-active' : ''}`}>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+      );
       <div
         className="nav-control"
         onClick={() => {
